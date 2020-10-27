dbb0ab0e1204d7a0861a92883e6531dab58f0e12
DonCorleone17 <mukebatu@gmail.com>
2020-10-28 02:45:06 +0700

    Initial commit

---
+++ system/resources/views/orama.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Imam Supriadi</title>
+</head>
+<body>
+<h2>Mulut Anda Kotor!</h2>
+<p>Anda tidak cocok untuk menjadi seorang pemimpin.</p>
+</body>
+</html>
